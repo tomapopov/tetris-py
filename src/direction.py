55177b8d331dea7cb9730d2eb450c08287487adb
tomapopov <toma98@hotmail.co.uk>
2024-08-09 13:44:44 +0100

    remove unused method

--- src/direction.py
+++ src/direction.py
@@ -8,21 +8,6 @@ class Direction(Enum):
     RIGHT = 2
     LEFT = 3
 
-
-
-    @classmethod
-    def from_char(cls, char: str):
-        mapping = {
-            "L": Direction.LEFT,
-            "R": Direction.RIGHT,
-            "D": Direction.DOWN,
-        }
-        if char not in mapping:
-            raise ValueError(f"Unsupported direction character {char!r}")
-        return mapping[char]
-
-
-
     @classmethod
     def from_command(cls, command: Command):
         mapping = {
